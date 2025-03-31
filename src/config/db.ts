e95a6fd16ea911dbbb41c9e4b24cef7f7f2f09a5
Morris Jesus <118037313+MorrisJesus@users.noreply.github.com>
2025-03-31 17:00:42 -0500

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,18 +2,23 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: "localhost",
-		dialect: "postgres",
-		port: 5433,
-		models: [ __dirname + "/../models/**/*"],
-		logging: false
-	},
+// const db = new Sequelize(
+// 	process.env.DB_NAME,
+// 	process.env.DB_USER,
+// 	process.env.DB_PASSWORD,
+// 	{
+// 		host: "localhost",
+// 		dialect: "postgres",
+// 		port: 5433,
+// 		models: [ __dirname + "/../models/**/*"],
+// 		logging: false
+// 	},
 	
-);
+// );
+
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*'],
+    logging: false
+})
 
 export default db;
